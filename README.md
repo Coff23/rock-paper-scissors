d2ab27a65030045a84b8d5996776fc7c591ac182
Joshua Coffey <itsjoshcoffey@gmail.com>
2023-06-02 17:15:40 -0500

    base of game done

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# rock-paper-scissors
+# Rock, Paper, Scissors Game - Event-Driven Application
+
+This is a real-time Rock, Paper, Scissors game application built using an event-driven architecture. The application consists of a hub server and two clients that connect to the hub server to play the game. The hub server moderates the game events, such as moves and determining the winner.
+
+## User Stories
+
+- As a player, I want to be able to connect to the game server as either Player 1 or Player 2, so that I can participate in the Rock, Paper, Scissors game.
+
+- As a player, I want to be able to choose my move (rock, paper, or scissors) and submit it to the server, so that I can play the game.
+
+- As a player, I want to receive the game results (winner or tie) after both players have made their moves, so that I know the outcome of each round.
+Problem Domain
+
+## The problem domain of this application is a real-time multiplayer game of Rock, Paper, Scissors. The challenge is to build a hub server that can handle multiple clients, assign them as players, receive and process their moves, determine the winner, and communicate the game results back to the players.
+
+### Technology Used
+
+- The Rock, Paper, Scissors game application is built using the following technologies:
+
+    - Node.js: The hub server and client applications are developed using Node.js, a JavaScript runtime.
+
+    - WebSocket: The WebSocket protocol is used for real-time bidirectional communication between the hub server and clients. The websocket library is used to implement WebSocket functionality.
+
+    - HTTP: An HTTP server is created to handle initial client connections and WebSocket upgrades.
+    
+    - JavaScript: The application logic is implemented using JavaScript, providing a flexible and event-driven programming language.
---
+++ hub.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const http = require('http');
+const socketIO = require('socket.io');
+
+// Create an HTTP server
+const server = http.createServer();
+const io = socketIO(server);
+
+// Store connected clients
+let player1 = null;
+let player2 = null;
+
+// Game variables
+let player1Move = null;
+let player2Move = null;
+
+// Handle incoming socket connections
+io.on('connection', (socket) => {
+  // Assign players when two clients are connected
+  if (!player1) {
+    player1 = socket;
+    player1.emit('message', 'You are Player 1');
+  } else if (!player2) {
+    player2 = socket;
+    player2.emit('message', 'You are Player 2');
+
+    // Start the game
+    startGame();
+  } else {
+    socket.emit('message', 'Game room is full');
+    socket.disconnect(true);
+  }
+
+  // Handle incoming moves from clients
+  socket.on('move', (move) => {
+    if (socket === player1) {
+      player1Move = move;
+      player1.emit('message', `You chose ${move}`);
+    } else if (socket === player2) {
+      player2Move = move;
+      player2.emit('message', `You chose ${move}`);
+    }
+
+    if (player1Move && player2Move) {
+      const result = determineWinner(player1Move, player2Move);
+      player1.emit('result', result);
+      player2.emit('result', result);
+
+      // Reset the game
+      resetGame();
+    }
+  });
+
+  // Handle client disconnection
+  socket.on('disconnect', () => {
+    // Reset the game if a player disconnects
+    if (socket === player1 || socket === player2) {
+      resetGame();
+    }
+  });
+});
+
+// Start the server
+const PORT = process.env.PORT || 3002;
+server.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
+
+// Game logic
+function startGame() {
+  player1.emit('message', 'Game starts! Make your move (rock, paper, or scissors)');
+  player2.emit('message', 'Game starts! Make your move (rock, paper, or scissors)');
+}
+
+function determineWinner(move1, move2) {
+  if (move1 === move2) {
+    console.log('It is a tie!');
+    return `It's a tie!`;
+  }
+
+  if (
+    (move1 === 'rock' && move2 === 'scissors') ||
+    (move1 === 'paper' && move2 === 'rock') ||
+    (move1 === 'scissors' && move2 === 'paper')
+  ) {
+    console.log('Player 1 wins!');
+    return 'Player 1 wins!';
+  }
+  console.log('Player 2 wins!');
+  return 'Player 2 wins!';
+}
+
+function resetGame() {
+  player1Move = null;
+  player2Move = null;
+  player1 = null;
+  player2 = null;
+}
